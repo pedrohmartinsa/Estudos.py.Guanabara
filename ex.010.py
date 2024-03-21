5a394bf58cb46319afd4bb55b4a6013d8db4cbaf
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-03-21 18:43:05 -0300

    Update ex.010.py

---
+++ ex.010.py
@@ -0,0 +1,5 @@
+altura = int(input('qual a altura da parede ?'))
+largura = int(input('qual a largura da parede ?'))
+area = altura * largura
+qnt = area / 2
+print(f'a quantidade de tinta necessária para pintar a parede é de {qnt} litros.')
