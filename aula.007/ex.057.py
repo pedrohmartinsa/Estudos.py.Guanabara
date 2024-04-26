38445e631cf6a651f718cde4226d5ac839e0fae5
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-04-25 23:01:46 -0300

    a

--- aula.007/ex.057.py
+++ aula.007/ex.057.py
@@ -1,1 +1,8 @@
-
+#SEXO
+a = 1
+while a == 'F' or 'M':
+    a = input('Qual seu genênero[F/M]?').upper().strip()
+    if a not in 'FM':
+        print('Errado, digite novamente.')
+    else:
+        print('Legal.')
