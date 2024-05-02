2563e284349dd40e66970bcfcf453545c1c4a7a1
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-05-02 19:26:19 -0300

    a

---
+++ aula.007/ex.061.py
@@ -0,0 +1,11 @@
+from time import sleep
+print('*' * 34)
+print('OS 10 PRIMEIROS TERMOS DE UMA P.A')
+print('*' * 34)
+
+p1 = int(input('Qual o primeiro termo ?'))
+r = int(input('Qual a razão?'))
+
+count = 10
+
+while count > 0:
