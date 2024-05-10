e10dbf4d8683aa1fc0bf116ebe3b1d689a1aff33
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-05-09 22:32:23 -0300

    consertando o 64 e o 65

--- aula.007/ex.064.py
+++ aula.007/ex.064.py
@@ -7,6 +7,6 @@ while i < 1:
     count += 1
     soma += n
     if n == 999:
-        print(f'Você digitou {count} números, e a soma entre eles é igual a {soma - 999}')
+        print(f'Você digitou {count - 1} números, e a soma entre eles é igual a {soma - 999}')
         print('FIM.')
         i += 1
--- aula.007/ex.065.py
+++ aula.007/ex.065.py
@@ -1,24 +1,31 @@
 count = 0
 soma = 0
 i = 0
+
 n = float(input('Digite um número: '))
-r = input('Quer continuar?[S/N] ').strip().upper()[0]
+r = str(input('Quer continuar?[S/N] ')).strip().upper()[0]
+
+menorNumero = n
+maiorNumero = n
 while i < 1:
-    menorNumero = n
-    maiorNumero = n
-    count += 1
-    soma += n
-    if n < menorNumero:
-        menorNumero = n
-    if n > maiorNumero:
-        maiorNumero = n
+
     if r in 'Ss':
+        count += 1
+        soma += n
         n = float(input('Digite um número: '))
         r = input('Quer continuar?[S/N] ').strip().upper()[0]
+
+    if n > maiorNumero:
+        maiorNumero = n
+
+    if n < menorNumero:
+        menorNumero = n
+
+
     if r in 'Nn':
+        count += 1
+        soma += n
         media = soma / count
         print(f'A média entre esses número é igual a: {media:.2f}')
         print(f'O maior número foi {maiorNumero} e o menor foi {menorNumero}')
         i += 1
-
-
