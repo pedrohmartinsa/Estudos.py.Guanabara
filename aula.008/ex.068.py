cd5df5a5bf2ab6870d2228d0019c8400a8f342ef
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-05-12 14:47:32 -0300

    a

--- aula.008/ex.068.py
+++ aula.008/ex.068.py
@@ -1,16 +1,46 @@
 from time import sleep
-from random import uniform
-print('[P] para par ou [I] para ímpar.')
-paridade = str(input('Jogador 1, você quer ser par ou ímpar ?')).strip().upper()
+from random import uniform #para escolher um número aleatorimanete
 
-if paridade in 'Ii':
-    print('Jogador 2, você é par.')
-else:
-    print('Jogador 2, você é ímpar.')
+count = 0
 
-comp = int(uniform(0, 10))
+while True:
+    comp = int(uniform(0, 10))
+    print()
 
-j1 = int(input('Escolha um número (de 0 a 10):'))
+    print('[P] para par ou [I] para ímpar.')
 
-if comp % 2 == 0 and j1 % 2 == 1:
-    
+    paridade = str(input('Jogador, você quer ser par ou ímpar ?')).strip().upper()
+    print()
+
+    while paridade not in 'Ii' or 'Pp':
+        print('Paridade inválida, digite novamente...')
+        print()
+        sleep(1.5)
+        print('[P] para par ou [I] para ímpar.')
+        paridade = str(input('Jogador, você quer ser par ou ímpar ?')).strip().upper()
+        print()
+
+        if paridade in 'Ii':
+            print('O computador é par.')
+            break
+
+        elif paridade in 'Pp':
+            print('O computador é ímpar.')
+            break
+
+
+
+    j1 = int(input('Escolha um número (de 0 a 10):'))
+    print()
+
+    if paridade in 'Ii' and (j1 + comp) % 2 == 1:
+        count += 1
+        print('Você venceu!')
+
+    elif paridade in 'Pp' and (j1 + comp) % 2 == 0:
+        count += 1
+        print('Você venceu!')
+
+    else:
+        print(f'Você perdeu depois de {count} vitórias.')
+        break
