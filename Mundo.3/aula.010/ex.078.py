c315579d42381b1000aa83ac9cab488bde61cf0e
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-05-22 22:48:26 -0300

    a

--- Mundo.3/aula.010/ex.078.py
+++ Mundo.3/aula.010/ex.078.py
@@ -1,17 +1,36 @@
 lista = []
-
+maiorIndex = []
+menorIndex = []
 for c in range(0, 5):
     lista.append(int(input('Digite um valor: ')))
 
 
 print()
+menor = lista[0]
+maior = lista[0]
 
 for i, v in enumerate(lista):
-    print(f'{i+1}º posição: {v}')
+    if v < menor:
+        menor = v
+
+    if v > maior:
+        maior = v
+
+    print(f'Posição {i}: {v}')
+
+for i, v in enumerate(lista):
+    if v == menor:
+        menorIndex.append(i)
+
+    if v == maior:
+        maiorIndex.append(i)
 
 
-lista.sort()
+
+
+
+
 print()
 print(f'Os valores digitados foram: {lista}')
-print(f'Maior valor digitado foi: {lista[0]}')
-print(f'Menor valor digitado foi: {lista[-1]}')
+print(f'Maior valor digitado foi: {maior} nas posições {maiorIndex}')
+print(f'Menor valor digitado foi: {menor} nas posições {menorIndex}')
