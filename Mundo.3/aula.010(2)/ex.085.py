1fe7d26c65b1b97f4461e452aeed2b37c6e67feb
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-05-24 19:22:19 -0300

    a

--- Mundo.3/aula.010(2)/ex.085.py
+++ Mundo.3/aula.010(2)/ex.085.py
@@ -6,7 +6,13 @@ for c in range(0, 7):
     n = int(input('Digite um valor: '))
     if n % 2 == 0:
         pares.append(n)
+
     elif n % 2 == 1:
         impares.append(n)
-    lista.append(pares and impares)
-print(lista)
+
+
+lista.append(impares)
+lista.append(pares)
+print()
+print(f'Pares: {lista[1]}')
+print(f'Impares: {lista[0]}')
