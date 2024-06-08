94d66a473c37bab1212803bcdb228bd567884f76
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-06-07 21:03:34 -0300

    a

--- Mundo.3/aula.010(2)/ex.087.py
+++ Mundo.3/aula.010(2)/ex.087.py
@@ -15,3 +15,16 @@ print('-='*30)
 print(f'[ {matriz[0][0]} ] [ {matriz[0][1]} ] [ {matriz[0][2]} ]')
 print(f'[ {matriz[1][0]} ] [ {matriz[1][1]} ] [ {matriz[1][2]} ]')
 print(f'[ {matriz[2][0]} ] [ {matriz[2][1]} ] [ {matriz[2][2]} ]')
+print('-='*30)
+pares = 0
+for v in matriz:
+    for valor in v:
+        if valor % 2 == 0:
+            pares += valor
+print(f'A soma dos números pares é: {pares}.')
+print(f'A soma dos valores da última coluna é: {matriz[0][2] + matriz[1][2] + matriz[2][2]}')
+maiorValor = 0
+for valores in matriz[1]:
+    if valores > maiorValor:
+        maiorValor = valores
+print(f'O maior valor é da segunda linha é {maiorValor}.')
---
+++ Mundo.3/aula.010(2)/ex.088.py
@@ -0,0 +1,15 @@
+from time import sleep
+from random import randint
+
+
+
+print('-' * 40)
+print(f'{"JOGA NA MEGA SENA":^40}')
+print('-' * 40)
+
+numeroDeJogos = int(input('Quantos jogos você quer que eu sorteie? '))
+
+print('-='*3, f'SORTEANDO {numeroDeJogos} JOGOS', '=-'*3)
+
+for c in range(0, numeroDeJogos):
+
