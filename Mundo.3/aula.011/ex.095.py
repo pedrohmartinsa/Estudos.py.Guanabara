7c5a0cc5fb298b83797c9f34fbbea027b8be52fc
Pedro Henrique Martins Alves dos Santos <pedroshenriquepsantos@gmail.com>
2024-06-24 21:09:38 -0300

    a

--- Mundo.3/aula.011/ex.095.py
+++ Mundo.3/aula.011/ex.095.py
@@ -8,6 +8,7 @@ while True:
     dados_jogador['num_partidas'] = int(input(f'Quantas partidas {dados_jogador['nome']} jogou?'))
     for c in range(0, dados_jogador['num_partidas']):
         gols.append(int(input(f'Quantos gols na partida {c+1}? ')))
+
     lista_jogadores.append(dados_jogador.copy())
     lista_gols.append(gols[:])
     dados_jogador.clear()
